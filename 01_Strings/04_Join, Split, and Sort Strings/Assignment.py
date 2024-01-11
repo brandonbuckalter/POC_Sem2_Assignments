fcdf5aa95eb498b5206f5b5d8518c16c92f50c69
Brandon Buckhalter <buckhbra001@warren.k12.in.us>
2024-01-11 09:58:27 -0500

    axax

---
+++ 01_Strings/04_Join, Split, and Sort Strings/Assignment.py
@@ -0,0 +1,11 @@
+
+
+first_name = input("what is your first name")
+last_name = input("what is your last name ")
+
+sep = " "
+full_name = sep.join(first_name ,last_name)  #YOUDO use the join function to 
+#join together first_name and last_name 
+print(full_name)
+
+
