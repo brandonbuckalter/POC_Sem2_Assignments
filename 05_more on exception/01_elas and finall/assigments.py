57e5bfa15c9fedd4b9c23cc0eaf0cd5db8e27002
Brandon Buckhalter <buckhbra001@warren.k12.in.us>
2024-02-01 10:01:20 -0500

    updeate

--- 05_more on exception/01_elas and finall/assigments.py
+++ 05_more on exception/01_elas and finall/assigments.py
@@ -2,20 +2,22 @@ number1 = 0
 number2 = 0
 try:
     number1 = int(input("Enter a number"))
-    # YOUDO.  use input function and int to set number2
+    number2 = int(input("Enter a number"))
 except ValueError:
-    # print message stating that an integer wasn't given
-    pass  # YOUDO remove pass when done
+     print("an integer wasn't given")  
+      
 else:
     print("No value error detected")
 finally:
     print("Values taken care of")
 
 try:
-    # YOUDO divide number1 / number2 and set to answer
-    # YOUDO  print the result of the division (aka answer with some helper text)
-    pass  # YOUDO remove pass when done
+    answer =(number1/number2)
+    print("answer equals")
+
+    
 except ZeroDivisionError:
-    # YOUDO:  print message stating that division by zero is not possible.
-    pass  # YOUDO remove pass when done
-#YOUDO:  else and finally here as well.  
+   print("Cannot divide by zero")
+
+   else:print("number were divdie")
+    pass 
