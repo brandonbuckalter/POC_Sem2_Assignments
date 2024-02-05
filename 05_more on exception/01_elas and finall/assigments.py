00339cb0119561fcb15acef50df9cfbcca50fc9d
Brandon Buckhalter <buckhbra001@warren.k12.in.us>
2024-02-05 09:28:00 -0500

    Brandon

--- 05_more on exception/01_elas and finall/assigments.py
+++ 05_more on exception/01_elas and finall/assigments.py
@@ -2,22 +2,23 @@ number1 = 0
 number2 = 0
 try:
     number1 = int(input("Enter a number"))
-    number2 = int(input("Enter a number"))
+    number2 = int(input("Enter a number greater then number one"))
 except ValueError:
-     print("an integer wasn't given")  
-      
+    print('an integer wwas not given')
+    
 else:
     print("No value error detected")
 finally:
     print("Values taken care of")
 
 try:
-    answer =(number1/number2)
-    print("answer equals")
-
-    
+    answer = (number1/number2)
+    print("answer equals" , answer)
+ 
 except ZeroDivisionError:
-   print("Cannot divide by zero")
+    print("Cannot divide by zero")
+else:
+    print('numbers were divible')
+finally:
+    
 
-   else:print("number were divdie")
-    pass 
